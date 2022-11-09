dec9b8901469fbe240f1201fe93c1d363b571c50
M Azhar Alauddin <80077273+zharmedia386@users.noreply.github.com>
2022-11-09 11:20:01 +0700

    Update Status Connection to MongoDB

--- server/server.js
+++ server/server.js
@@ -63,6 +63,6 @@ app.all('*', (req, res) => {
 app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB Database');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
