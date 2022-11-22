f7bc213e856a9e946c06ce923650602ff37b0e2c
SalmaSyawalan <salma.syawalan@gmail.com>
2022-11-22 20:47:10 +0700

    alignment

--- client/src/views/CMS/ChapterList.vue
+++ client/src/views/CMS/ChapterList.vue
@@ -1,14 +1,16 @@
 <template>
   <v-container>
-    <v-card >
+    <v-btn class="d-flex align-start flex-column">Create New Chapter</v-btn>
+    <br>
+    <v-card>
       <v-card-title>Chapter List</v-card-title>
-      <v-card-text v-for="(chapter, index) in chapters" v-bind:key="index">
+      <v-card-text v-for="(chapter, index) in chapters" v-bind:key="index" class="d-flex justify-space-between auto" :color="$vuetify.theme.dark ? 'grey lighten-5' : 'white'" flat tile>
         <v-icon>{{chapter.icon}}</v-icon> {{chapter.title}}
         <v-btn outlined fab small><v-icon>mdi-pencil</v-icon></v-btn>
         <v-btn outlined fab small><v-icon>mdi-delete</v-icon></v-btn>
       </v-card-text>
     </v-card>
-    <v-btn>Create New Chapter</v-btn>
+    
   </v-container>
 </template>
 
--- client/src/views/CMS/SectionList.vue
+++ client/src/views/CMS/SectionList.vue
@@ -1,14 +1,19 @@
 <template>
   <v-container>
+    <v-btn class="d-flex align-start flex-column" to="/cms/section/create">Create New Section</v-btn>
+    <br>
     <v-card>
       <v-card-title>Section List</v-card-title>
       <v-card-text v-for="(section, index) in sections" v-bind:key="index">
-        {{section}}
-        <v-btn outlined fab small :to="`/cms/section/edit-${section}`"><v-icon>mdi-pencil</v-icon></v-btn>
-        <v-btn outlined fab small><v-icon>mdi-delete</v-icon></v-btn>
+        <v-card class="d-flex justify-space-between auto" :color="$vuetify.theme.dark ? 'grey lighten-5' : 'white'" flat tile>
+          {{section}}
+          <div>
+            <v-btn outlined fab small :to="`/cms/section/edit-${section}`"><v-icon>mdi-pencil</v-icon></v-btn>
+            <v-btn outlined fab small><v-icon>mdi-delete</v-icon></v-btn>
+          </div>
+        </v-card>
       </v-card-text>
     </v-card>
-    <v-btn to="/cms/section/create">Create New Section</v-btn>
   </v-container>
 </template>
 
