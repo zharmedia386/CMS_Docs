8c171db68de1e1a54a2fc61ae2fea5ca9d6983d4
SalmaSyawalan <salma.syawalan@gmail.com>
2022-11-23 09:07:12 +0700

    fix chapter and section layout

--- client/src/views/CMS/ChapterList.vue
+++ client/src/views/CMS/ChapterList.vue
@@ -1,14 +1,20 @@
 <template>
   <v-container>
-    <v-btn class="d-flex align-start flex-column">Create New Chapter</v-btn>
+    
     <br>
     <v-card>
-      <v-card-title>Chapter List</v-card-title>
-      <v-card-text v-for="(chapter, index) in chapters" v-bind:key="index">
+      <v-card-title class="d-flex justify-space-between px-5">
+        Chapter List
+        <v-btn>Create New Chapter</v-btn>
+      </v-card-title>
+      <br>
+      <v-card-text class="d-flex justify-space-between px-5" v-for="(chapter, index) in chapters" v-bind:key="index">
         <!-- <v-icon>{{chapter.icon}}</v-icon> {{chapter.title}} -->
         {{ chapter.title }}
-        <v-btn outlined fab small><v-icon>mdi-pencil</v-icon></v-btn>
-        <v-btn outlined fab small><v-icon>mdi-delete</v-icon></v-btn>
+        <div>
+          <v-btn outlined fab small><v-icon>mdi-pencil</v-icon></v-btn>
+          <v-btn outlined fab small><v-icon>mdi-delete</v-icon></v-btn>
+        </div>
       </v-card-text>
     </v-card>
     
--- client/src/views/CMS/SectionList.vue
+++ client/src/views/CMS/SectionList.vue
@@ -1,17 +1,20 @@
 <template>
   <v-container>
-    <v-btn class="d-flex align-start flex-column" to="/cms/section/create">Create New Section</v-btn>
+    
     <br>
     <v-card>
-      <v-card-title>Section List</v-card-title>
-      <div class="text-left" v-for="(section, index) in sections" v-bind:key="index">
-        <v-card-subtitle class="d-inline" v-text="section.title"></v-card-subtitle>
-        <v-card-text class="d-inline">
+      <v-card-title class="d-flex justify-space-between px-5">
+        Section List
+        <v-btn to="/cms/section/create">Create New Section</v-btn>
+      </v-card-title>
+      <br>
+      <v-card-text class="d-flex justify-space-between px-5 text-left" v-for="(section, index) in sections" v-bind:key="index">
+        <v-card-subtitle v-text="section.title"></v-card-subtitle>
+        <div>
           <v-btn outlined fab small :to="`/cms/section/${section._id}`"><v-icon>mdi-pencil</v-icon></v-btn>
           <v-btn outlined fab small @click="deleteSection(section._id)"><v-icon>mdi-delete</v-icon></v-btn>
-        </v-card-text>
-        
-      </div>
+        </div>
+      </v-card-text>
       <!-- <v-card-text class="text-left" v-for="(section, index) in sections" v-bind:key="index">
         <p class="text-left d-inline" v-text="section.title"></p>
         <p class="text-right d-inline">
