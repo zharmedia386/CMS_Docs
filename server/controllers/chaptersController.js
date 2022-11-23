84ee95b0987ef2a798db2e4a745cfa75edb70e6c
mfauzirh <mfauzirizkih@gmail.com>
2022-11-23 08:52:45 +0700

    menambahkan fitur insert chapter dan update chapter

--- server/controllers/chaptersController.js
+++ server/controllers/chaptersController.js
@@ -66,8 +66,8 @@ const createNewChapter = async (req, res) => {
 const updateChapter = async (req, res) => {
     if (!req?.body?.id) return res.status(400).json({ 'message': 'Chapters ID required.' });
 
-    if (!req?.body?.title || !req?.body?.version) {
-        return res.status(400).json({ 'message': 'Title, documentation, and version is required' });
+    if (!req?.body?.title) {
+        return res.status(400).json({ 'message': 'Title is required' });
     }
 
     let chapterId = new mongo.ObjectId(req.body.id)
@@ -77,7 +77,6 @@ const updateChapter = async (req, res) => {
 
     const chapter = {
         title: req.body.title,
-        version: req.body.version,
         updatedAt: new Date() 
     }
 
@@ -91,8 +90,8 @@ const updateChapter = async (req, res) => {
         // updating chapter title in documentation content
         await Documentation.updateOne(
             {},
-            { $set: { "content.$[ct].chapter.$[ch].title": `${chapter.title}` } },
-            { arrayFilters: [ { "ct.version": chapter.version[0] }, { "ch._id": chapterId } ] }
+            { $set: { "content.$[].chapter.$[ch].title": `${chapter.title}` } },
+            { arrayFilters: [ { "ch._id": chapterId } ] }
         )
     } catch (error) {
         res.status(400).send({ message: error.message })
