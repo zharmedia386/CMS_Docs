898d4b2bb72488f4745e19eeacff465925f2f07e
RafliLM <raflilutfiansyah28@gmail.com>
2022-12-07 00:57:27 +0700

    Page Title and routing improvization

---
+++ client/src/views/Auth/LoginView.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-card max-width="500" class="container">
+        <v-card-title>Login</v-card-title>
+        <v-form v-model="form.valid" lazy-validation>
+          <v-text-field
+            v-model="form.email"
+            label="E-mail"
+            :rules="form.emailRules"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="form.password"
+            label="Password"
+            :rules="form.passwordRules"
+            required
+          ></v-text-field>
+          <v-btn>
+            login
+          </v-btn>
+        </v-form>
+      </v-card>
+    </v-main>
+  </v-app>
+  
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      form : {
+        email : "",
+        emailRules: [
+          v => !!v || 'E-mail is required',
+          v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+        ],
+        password : "",
+        passwordRules: [
+          v => !!v || 'Password is required',
+          v => v.length >= 8 || 'Min 8 characters'
+        ],
+        valid : true
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
