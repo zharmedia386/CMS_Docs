4109238010035f60494a45eee5934075d0745333
M Azhar Alauddin <80077273+zharmedia386@users.noreply.github.com>
2022-12-06 16:06:01 +0700

    vercel conf

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
     'http://localhost:3500',
     'http://localhost:3000',
     'http://localhost:8080',
+    'https://topoci-nasution.com'
 ];
 
 module.exports = allowedOrigins;
