a3a3b26ed3eb956ec57636c38eb2d08789a6b076
M Azhar Alauddin <80077273+zharmedia386@users.noreply.github.com>
2023-03-16 09:15:22 +0700

    Mogenius Deployment Config

--- client/src/main.js
+++ client/src/main.js
@@ -19,7 +19,8 @@ Vue.config.productionTip = false
 
 // Vue.prototype.$apiuri = "https://cms-docs-prod-cms-docs-hds0vk.mo2.mogenius.io"
 // Vue.prototype.$apiuri = "https://cmsdocs-production.up.railway.app"
-Vue.prototype.$apiuri = "http://localhost:3500"
+// Vue.prototype.$apiuri = "http://localhost:3500"
+Vue.prototype.$apiuri = "https://cms-docs-prod-cms-docs-hds0vk.mo2.mogenius.io"
 
 
 new Vue({
