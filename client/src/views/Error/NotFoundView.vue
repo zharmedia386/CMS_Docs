cc0c19550ec685de4fd7970f2240a95f01708a3a
mfauzirh <mfauzirizkih@gmail.com>
2023-04-02 21:09:43 +0700

    implement not found page

--- client/src/views/Error/NotFoundView.vue
+++ client/src/views/Error/NotFoundView.vue
@@ -1,36 +1,60 @@
 <template>
-    <div class="d-flex justify-center align-center" style="width: 100%; height: 100%;">
+    <div class="not-found-container d-flex justify-center align-center" style="width: 100%; height: 100%;">
         <v-container>
-            <h1 class="linear-wipe">404 Page Not Found</h1>
+            <img 
+                src="@/assets/images/404.png" 
+                alt="Not Found" 
+                :class="{ 'not-found-mobile': this.$vuetify.breakpoint.mobile }"
+            >
+            <h1 
+                class="white--text" 
+                :class="{ 'not-found-h1': this.$vuetify.breakpoint.mobile }"
+            >
+                Oops! This Page is Not Found
+            </h1>
+            <h2
+                :class="{ 'not-found-h2': this.$vuetify.breakpoint.mobile }"
+            >
+                We can’t find the page that you’re looking for
+            </h2>
+            <v-btn 
+                class="animated-gradient white--text mt-7"
+                @click="backToHomePage"
+            >
+                Back To Home
+            </v-btn>
         </v-container>
     </div>
 </template>
 
-<style scoped>
-.linear-wipe {
-    --bg-size: 400%;
-    font-weight: bold;
-    background: linear-gradient(90deg,
-            var(--secondary-purple),
-            var(--secondary-blue),
-            var(--secondary-purple)) 0 0 / var(--bg-size) 100%;
-    color: transparent;
-    background-clip: text;
-}
-
-@media (prefers-reduced-motion: no-preference) {
-    .linear-wipe {
-        animation: move-bg 8s linear infinite;
-    }
-
-    @keyframes move-bg {
-        to {
-            background-position: var(--bg-size) 0;
+<script>
+export default {
+    methods: {
+        backToHomePage(){
+            this.$router.push({ name: 'docs' });
         }
     }
 }
+</script>
 
-h1 {
-    font-size: 5rem;
+<style scoped>
+img {
+    width: 50%;
+}
+
+h2 {
+    color: #AEAEAE;
+    font-weight: normal;
+}
+
+.not-found-mobile {
+    width: 80%;
+}
+
+.not-found-h1 {
+    font-size: 1.5rem;
+}
+.not-found-h2 {
+    font-size: 1rem;
 }
 </style>
