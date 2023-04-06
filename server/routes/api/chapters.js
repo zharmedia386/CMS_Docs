25d380f36362a2bcd5877d067b6820820662acff
M Azhar Alauddin <80077273+zharmedia386@users.noreply.github.com>
2023-04-06 11:11:25 +0700

    Update Create Chapters Validator

--- server/routes/api/chapters.js
+++ server/routes/api/chapters.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const chaptersController = require('../../controllers/chaptersController')
 const { verifyJWT } = require("../../middleware/verifyJWT")
-const { validateCreateUser } = require("../../validators/chaptersValidator")
+const { validateCreateUser, validate } = require("../../validators/chaptersValidator")
 
 router.route('/')
     .get(chaptersController.getAllChapters)
-    .post(verifyJWT, validateCreateUser, chaptersController.createNewChapter)
+    .post(verifyJWT, validateCreateUser, validate, chaptersController.createNewChapter)
     .put(verifyJWT, chaptersController.updateChapter)
     .delete(verifyJWT, chaptersController.deleteChapter);
 
--- server/validators/chaptersValidator.js
+++ server/validators/chaptersValidator.js
@@ -1,4 +1,4 @@
-const { body } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 const validateCreateUser = [
   body('title').isString().withMessage('title must be a string'),
@@ -6,4 +6,12 @@ const validateCreateUser = [
   body('version').isString().withMessage('version must be an array'),
 ];
 
-module.exports = { validateCreateUser };
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  };
+
+module.exports = { validateCreateUser, validate };
