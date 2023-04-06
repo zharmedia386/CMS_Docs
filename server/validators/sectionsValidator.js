84768d4acc92705e65587209d2c6c7119a3361af
mfauzirh <mfauzirizkih@gmail.com>
2023-04-06 13:38:28 +0700

    add validator for getSection by id, all, and create new section

---
+++ server/validators/sectionsValidator.js
@@ -0,0 +1,26 @@
+const { body, param } = require('express-validator');
+
+const getSectionByIdRules = [
+    param('id')
+        .notEmpty().withMessage("id is required").bail()
+        .isString().withMessage("id must be a string").bail()
+        .isLength({ min: 24, max: 24 }).withMessage("id must have length of 24 characters")
+];
+
+const createSectionRules = [
+    body('title')
+        .notEmpty().withMessage("title is required").bail()
+        .isString().withMessage("id must be a string"),
+    body('content')
+        .notEmpty().withMessage("content is required").bail()
+        .isString().withMessage("content must be a string"),
+    body('alias')
+        .optional()
+        .notEmpty().withMessage("content is required").bail()
+        .isString().withMessage("content must be a string")
+];
+
+module.exports = {
+    getSectionByIdRules,
+    createSectionRules
+};
