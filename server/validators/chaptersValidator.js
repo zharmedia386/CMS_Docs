d50f59c8c0329fba86370eb9552db7670631836d
M Azhar Alauddin <80077273+zharmedia386@users.noreply.github.com>
2023-04-06 11:40:04 +0700

    Update ChapterValidator

--- server/validators/chaptersValidator.js
+++ server/validators/chaptersValidator.js
@@ -1,17 +1,9 @@
-const { body, validationResult } = require('express-validator');
+const { body } = require('express-validator');
 
-const validateCreateUser = [
-  body('title').isString().withMessage('title must be a string'),
-  body('documentationId').isString().withMessage('documentationId must be a string'),
-  body('version').isString().withMessage('version must be an array'),
+const createUserRules = [
+  body('title').notEmpty().withMessage('title is required').isString().withMessage('title must be a string'),
+  body('documentationId').notEmpty().withMessage('documentationId is required').isString().withMessage('documentationId must be a string'),
+  body('version').notEmpty().withMessage('version is required').isString().withMessage('version must be an array'),
 ];
 
-const validate = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    }
-    next();
-  };
-
-module.exports = { validateCreateUser, validate };
+module.exports = { createUserRules };
