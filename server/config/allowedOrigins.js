02587acd44de9cc0e007ed2048b636302ffe7b6d
mfauzirh <mfauzirizkih@gmail.com>
2023-04-09 06:28:00 +0000

    add config server linux

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,12 +1,8 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'http://localhost:3000',
     'http://localhost:8080',
-    'http://localhost:8081',
-    'https://topoci-nasution.com',
-    'https://cms-docs.netlify.app'
+    'https://cms-docs.netlify.app',
+    'http://172.16.88.130:3500'
 ];
 
 module.exports = allowedOrigins;
---
+++ server/ecosystem.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  apps : [{
+    name   : "serverBackend",
+    script : "npm run start"
+  }]
+}
